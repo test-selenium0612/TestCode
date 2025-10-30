8223f79b21d417a79841e2ae4ead2eb4c754aec6
Shubham_Joshi <Shubham1.joshi1@gmail.com>
2025-10-30 23:08:49 +0530

    adding from usa

--- src/myProjectGit/GitProject.java
+++ src/myProjectGit/GitProject.java
@@ -3,7 +3,9 @@ package myProjectGit;
 public class GitProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.print("added from usa 1");
+		System.out.print("added from usa 1 for india");
 
 	}
 
